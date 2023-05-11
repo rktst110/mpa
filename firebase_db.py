58103b47f7811bc52d710b170f4c950bac38d059
rktst110 <84590431+rktst110@users.noreply.github.com>
2023-05-11 16:13:48 +0530

    s

--- firebase_db.py
+++ firebase_db.py
@@ -5,17 +5,6 @@ from google.cloud import firestore
 
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("nse-data-saving-2023-5hf5fgdrg-firebase-adminsdk-yzdk6-d335b442d8.json")
-"""
-# Create a reference to the Google post.
-doc_ref = db.collection("posts").document("Google")
-
-# Then get the data at that reference.
-doc = doc_ref.get()
-
-# Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
-"""
 
 
 #doc_ref = db.collection("April 2023").document("13 April 2023")
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # some other page in your web application
 
-from firebase import get_user_data
+#from firebase import get_user_data
+import firebase_db
 
 # get user data for user with ID 123
 user_data = get_user_data('123')
