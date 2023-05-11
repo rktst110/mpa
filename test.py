555588bd8da1f78958960ace007553c096710bd8
rktst110 <84590431+rktst110@users.noreply.github.com>
2023-05-11 17:51:55 +0530

    d

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 # some other page in your web application
 
 #from firebase import get_user_data
-import firebase_db
+import firebase_db as fdb
 
 # get user data for user with ID 123
-user_data = get_user_data('123')
+user_data = fdb.get_user_data('123')
 
 # do something with the user data...
